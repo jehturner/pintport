080de696366d6e162155256f3fbf43c35218ed73
James E.H. Turner <jturner@gemini.edu>
2018-09-26 22:20:41 -0300

    Add a simple setup.py and Python module subdir.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pintport',
+      version='0.1',
+      description='Simple international portfolio analysis in Python',
+      author='James Turner',
+      packages=['pintport'],
+     )
